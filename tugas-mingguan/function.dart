97923e77e39561234070db6eae4695167d099bb2
viriyahartati <viriyahartati@gmail.com>
2024-01-13 12:40:17 +0700

    tugas mingguan 1

---
+++ tugas-mingguan/function.dart
@@ -0,0 +1,39 @@
+void main(List<Strng> args) {
+  teriak();
+}
+
+terial() {
+  print("Hello sanbers");
+}
+
+perkalian(num1, num2) {
+  return num1 * num2;
+}
+
+introduce(){
+  var name = "Agus";
+var age = 30;
+var address = "Jln. Malioboro, Yogyakarta";
+var hobby = "Gaming";
+ 
+var perkenalan = (name, age, address, hobby);
+print(perkenalan) // Menampilkan "Nama saya Agus, umur saya 30 tahun, alamat saya di Jln. Malioboro, Yogyakarta,dan saya punya hobby yaitu Gaming!" 
+
+}
+
+//soal 4
+// int hitungFaktorial(int angka) {
+//   if (angka <= 0) {
+//     return 1;
+//   } else {
+//     return angka * hitungFaktorial(angka - 1);
+//   }
+// }
+
+// void main() {
+//   int angka = 6;
+//   int hasilFaktorial = hitungFaktorial(angka);
+
+//   print('$angka! = $hasilFaktorial');
+// }
+
---
+++ tugas-mingguan/looping.dart
@@ -0,0 +1,63 @@
+// soal 1
+
+// void main() {
+//   print("looping pertama : ");
+
+//   int i = 0;
+//   while (i <= 20) {
+//     print("$i - i love coding");
+//     i += 2;
+//   }
+// }
+
+// print("looping kedua");
+// int j = 20;
+// while(j <= 0){
+//   print("$j - I will become a mobile developer");
+//   j -=2;
+// }
+
+// soal 2
+//A. Jika angka ganjil maka tampilkan Santai
+//B. Jika angka genap maka tampilkan Berkualitas
+//C. Jika angka yang sedang ditampilkan adalah kelipatan 3 DAN angka ganjil maka tampilkan I Love Coding.
+// void main() {
+//   for (int i = 0; i <= 20; i++) {
+//     if (i % 2 == 1) {
+//       print("$i santai");
+//     } else if (i % 2 == 0) {
+//       print("$i berkualitas");
+//     } else if (i % 3 == 0 && i % 2 == 1) {
+//       print("$i I love coding");
+//     }
+//   }
+// }
+
+//soal 3
+// void main() {
+//   int row, col;
+//   for (row = 1; row <= 8; row++) {
+//     for (col = 1; col <= 4; col++) {
+//       print("#");
+//     }
+//     print(" ");
+//   }
+// }
+
+// void main() {
+//   int r = 1;
+//   int c = 0;
+//   int n = 5;
+//   while (r <= n) {
+//     while (c <= r - 1) {
+//       print("#");
+//       c++;
+//     }
+//     r++;
+//     c = 0;
+//     print(" ");
+//   }
+// }
+
+
+
